7c07c77c5e1a3108deb77ad2a1f8bd8b6459c57e
raulvillares <raulvillares@gmail.com>
2017-09-21 10:08:59 +0200

    TEST 2 / peorPartida (VERDE): Refactorización.

--- src/test/Test.kt
+++ src/test/Test.kt
@@ -1,22 +1,17 @@
 package com.raulvillares.kotlin.katas.bolos.test
 
 import com.raulvillares.kotlin.katas.bolos.produccion.Partida
-import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class BolosTest {
-
-    @Test
-    fun crearPartida() {
-        val partida = Partida()
-    }
 
     @Test
     fun peorPartida() {
         val partida = Partida()
         partida.tirar(0)
         for(tirada in 1..20) partida.tirar(0)
-        Assertions.assertEquals(0, partida.resultado)
+        assertEquals(0, partida.resultado)
     }
 
 }

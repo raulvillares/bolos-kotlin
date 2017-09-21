9a7ed0079105aae139324e453bc18e3355fb78e5
raulvillares <raulvillares@gmail.com>
2017-09-21 15:05:26 +0200

    TEST 4 / unSpare (VERDE): Se detecta el semipleno y se suman los puntos extra.

--- src/main/Partida.kt
+++ src/main/Partida.kt
@@ -3,7 +3,15 @@ package com.raulvillares.kotlin.katas.bolos.produccion
 class Partida {
     private val tiradas = mutableListOf<Int>()
     val  resultado : Int
-        get() = tiradas.sum()
+        get() {
+            var total = 0
+            for(i in 0..19 step 2) {
+                val bolosDerribadosTurno = tiradas[i] + tiradas[i+1]
+                total += bolosDerribadosTurno
+                if(bolosDerribadosTurno == 10) total += tiradas[i+2]
+            }
+            return total
+        }
 
     fun tirar(bolosDerribados : Int, veces : Int = 1) {
         for (tirada in 1..veces) this.tiradas.add(bolosDerribados)

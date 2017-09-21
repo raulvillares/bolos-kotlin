dc255c4718e833f93c005c3cff73dd7eb193681f
raulvillares <raulvillares@gmail.com>
2017-09-21 10:04:40 +0200

    TEST 2 / peorPartida (ROJO) : Falta implementar funcion tirar en Partida.

--- src/test/Test.kt
+++ src/test/Test.kt
@@ -1,6 +1,7 @@
 package com.raulvillares.kotlin.katas.bolos.test
 
 import com.raulvillares.kotlin.katas.bolos.produccion.Partida
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 
 class BolosTest {
@@ -8,6 +9,12 @@ class BolosTest {
     @Test
     fun crearPartida() {
         val partida = Partida()
+    }
+
+    @Test
+    fun peorPartida() {
+        val partida = Partida()
+        partida.tirar(0)
     }
 
 }

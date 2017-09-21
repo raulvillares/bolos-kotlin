693ac1e98a74926664093a2737ef34469ff4f103
raulvillares <raulvillares@gmail.com>
2017-09-21 10:28:53 +0200

    TEST 3 / todoUnos (VERDE) : Propiedad resultado acumula tiradas.

--- src/main/Partida.kt
+++ src/main/Partida.kt
@@ -1,6 +1,8 @@
 package com.raulvillares.kotlin.katas.bolos.produccion
 
 class Partida {
-    val resultado: Int = 0
-    fun tirar(bolosDerribados : Int) {}
+    var resultado : Int = 0
+    fun tirar(bolosDerribados : Int) {
+        this.resultado += bolosDerribados
+    }
 }

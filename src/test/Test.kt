963991b2458cdad5b2fbc76baa7eaf1041df6265
raulvillares <raulvillares@gmail.com>
2017-09-21 10:21:10 +0200

    TEST 3 / todoUnos (ROJO) : Resultado esperado 20 pero obtenido 0.

--- src/test/Test.kt
+++ src/test/Test.kt
@@ -9,9 +9,15 @@ class BolosTest {
     @Test
     fun peorPartida() {
         val partida = Partida()
-        partida.tirar(0)
         for(tirada in 1..20) partida.tirar(0)
         assertEquals(0, partida.resultado)
+    }
+
+    @Test
+    fun todoUnos() {
+        val partida = Partida()
+        for(tirada in 1..20) partida.tirar(1)
+        assertEquals(20, partida.resultado)
     }
 
 }

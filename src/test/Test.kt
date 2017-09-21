4d15ed1221cbb2d95a41879b5b43c1d3c6a6b0e0
raulvillares <raulvillares@gmail.com>
2017-09-21 11:35:29 +0200

    TEST 3 / todoUnos (VERDE) : Refactorización. Extracción de la creación de partidas.

--- src/test/Test.kt
+++ src/test/Test.kt
@@ -2,20 +2,28 @@ package com.raulvillares.kotlin.katas.bolos.test
 
 import com.raulvillares.kotlin.katas.bolos.produccion.Partida
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
 class BolosTest {
 
+    lateinit var partida : Partida
+
+    @BeforeEach
+    fun crearPartida() {
+        partida = Partida()
+    }
+
     @Test
     fun peorPartida() {
-        val partida = Partida()
+        crearPartida()
         for(tirada in 1..20) partida.tirar(0)
         assertEquals(0, partida.resultado)
     }
 
     @Test
     fun todoUnos() {
-        val partida = Partida()
+        crearPartida()
         for(tirada in 1..20) partida.tirar(1)
         assertEquals(20, partida.resultado)
     }
